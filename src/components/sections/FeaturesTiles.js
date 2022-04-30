b4e10c039906d2a52f6fbdf6e857b288e8a85f4e
David Lee <david.lee7731@yahoo.com>
2022-04-30 13:24:12 -0400

    initial commit

---
+++ src/components/sections/FeaturesTiles.js
@@ -0,0 +1,203 @@
+import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../../utils/SectionProps';
+import SectionHeader from './partials/SectionHeader';
+import Image from '../elements/Image';
+
+const propTypes = {
+  ...SectionTilesProps.types
+}
+
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+const FeaturesTiles = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'features-tiles section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'features-tiles-inner section-inner pt-0',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap center-content',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'See the whole picture',
+    paragraph: 'Visualizations of key metrics ready the cluster level.'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-01.svg')}
+                      alt="Features tile icon 01"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                  GUI Monitoring Tool
+                    </h4>
+                  <p className="m-0 text-sm">
+                  An intuitive app to facilitate monitoring
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-02.svg')}
+                      alt="Features tile icon 02"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                  Advanced Reporting
+                    </h4>
+                  <p className="m-0 text-sm">
+                  Enhance Kafka’s built-in reporting features
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-03.svg')}
+                      alt="Features tile icon 03"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                  Smart Suggestions
+                    </h4>
+                  <p className="m-0 text-sm">
+                  Offers advanced insights on bottlenecks and cluster performance
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-04.svg')}
+                      alt="Features tile icon 04"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Robust System
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Informative details on performance of brokers
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-05.svg')}
+                      alt="Features tile icon 05"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Topic-Heavy
+                    </h4>
+                  <p className="m-0 text-sm">
+                  Statistics and activity details at the topic level
+                    </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require('./../../assets/images/feature-tile-icon-06.svg')}
+                      alt="Features tile icon 06"
+                      width={64}
+                      height={64} />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Headache-Free Reporting
+                    </h4>
+                  <p className="m-0 text-sm">
+                    Monitoring ZooKeeper infrastructure within one interface
+                    </p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+FeaturesTiles.propTypes = propTypes;
+FeaturesTiles.defaultProps = defaultProps;
+
+export default FeaturesTiles;
